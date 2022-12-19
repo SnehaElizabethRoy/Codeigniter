8614153528f591f00d044d4654a1e6bf03f046fc
snehaelizabethroy <elizabethsneha@gmail.com>
2022-12-19 16:36:33 +0530

    button RED

--- application/controllers/Hello.php
+++ application/controllers/Hello.php
@@ -5,7 +5,7 @@ class Hello extends CI_Controller {
       
     public function index()  
     {  
-        $this->load->view('helloworld');  
+        $this->load->view('hey world');  
     }  
 }  
 ?>  
